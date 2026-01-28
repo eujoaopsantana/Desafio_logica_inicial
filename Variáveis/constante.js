a1ad31184116b8bd41716c160b963fa2c3c7fac5
João Pedro <128496417+eujoaopsantana@users.noreply.github.com>
2026-01-27 23:30:48 -0300

    Delete Variáveis directory

--- Variáveis/constante.js
+++
@@ -1,6 +0,0 @@
-//Declarando constantes
-const notificacao = "Pokemon GO Diz:"
-
-//saída
-console.log (notificacao + " tem um novo pokemon na região")
-console.log (notificacao + " você foi derrotado por um líder")
